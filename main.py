efb80cc354833d29c0548e48f30e8b17076e1bc1
Hafnium780 <allenli780@gmail.com>
2023-09-26 20:19:40 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,51 +1,89 @@
-from Tkinter import *
-import ttk
+from time import sleep
+import Tkinter as tk
+import ScrolledText as st
 import pyscf
 import threading
 from pyscf import gto, scf
 
-def pyscfThread():
-    mol = gto.Mole()
-    mol.atom = xyzInput.get()
-    mol.basis = 'cc-pVTZ'
-    mol.verbose = 5
-    mol.output = 'output.txt'
-    mol.build()
+queue = []
 
-    mf = scf.RHF(mol)
-    mf.kernel()
+def pyscfThread():
+    while (runThread):
+        print(str(len(queue)) + " molecules in queue")
+        if (len(queue) > 0):
+            mol = gto.Mole()
+            mol.atom = queue[0]
+            queue.pop(0)
+            print(mol.atom)
+            sleep(3)
+            # mol.basis = 'cc-pVTZ'
+            # mol.verbose = 5
+            # mol.output = 'output.txt'
+            # mol.build()
+
+            # mf = scf.RHF(mol)
+            # mf.kernel()
+            print("Completed computation")
+        sleep(1)
     
 
-def calculate(*args):
-    x = threading.Thread(target=pyscfThread)
-    x.start()
+def addToQueue():
+    queue.append(xyzInput.get("1.0", tk.END))
+
+def endThread():
+    global runThread
+    runThread = False
+    root.destroy()
 
 
-root = Tk()
+
+
+runThread = True
+pythread = threading.Thread(target=pyscfThread)
+pythread.start()
+
+root = tk.Tk()
 root.title("PySCF")
+root.protocol("WM_DELETE_WINDOW", endThread)
 
-mainframe = ttk.Frame(root, padding="3 3 12 12")
-mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
+w, h = root.winfo_screenwidth(), root.winfo_screenheight()
+root.geometry("%dx%d" % (w, h))
+
 root.columnconfigure(0, weight=1)
+root.columnconfigure(1, weight=3)
 root.rowconfigure(0, weight=1)
+root.rowconfigure(1, weight=4)
 
-xyzInput = StringVar()
-xyzInput = ttk.Entry(mainframe, width=7, textvariable=xyzInput)
-xyzInput.grid(column=1, row=1, sticky=(W, E))
+# Frames
 
-energyOutput = StringVar()
-ttk.Label(mainframe, textvariable=energyOutput).grid(column=1, row=2, sticky=(W, E))
+xyzInputFrame = tk.Frame(root, background='red')
+settingsFrame = tk.Frame(root, background='blue')
+queueFrame = tk.Frame(root, background='green')
+outputFrame = tk.Frame(root, background='black')
+xyzInputFrame.pack_propagate(0)
+settingsFrame.grid_propagate(0)
+queueFrame.grid_propagate(0)
+outputFrame.grid_propagate(0)
 
-ttk.Button(mainframe, text="Calculate", command=calculate).grid(
-    column=1, row=3, sticky=W)
+# XYZ Input
+xyzInputFrame.rowconfigure(0, weight=3)
+xyzInputFrame.rowconfigure(1, weight=1)
 
-for child in mainframe.winfo_children():
-    child.grid_configure(padx=5, pady=5)
+xyzInput = st.ScrolledText(xyzInputFrame)
+xyzButton = tk.Button(xyzInputFrame, text="Compute", command=addToQueue)
 
-xyzInput.focus()
-# root.bind("<Return>", calculate)
+xyzButton.pack(padx=4, pady=4, side="bottom", fill="both")
+xyzInput.pack(padx=4, pady=4, side="top", fill="x")
+
+
+xyzInputFrame.grid(column=0, row=0, sticky="nesw")
+settingsFrame.grid(column=1, row=0, sticky="nesw")
+queueFrame.grid(column=0, row=1, sticky="nesw")
+outputFrame.grid(column=1, row=1, sticky="nesw")
+
 
 root.mainloop()
+
 
 
 # mol = gto.Mole()
