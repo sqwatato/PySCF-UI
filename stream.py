255b06f43bdf943864644087262911c8a9075cd6
Hafnium780 <allenli780@gmail.com>
2023-10-28 15:41:13 -0700

    Add a simple synchronous queue

--- stream.py
+++ stream.py
@@ -1,9 +1,14 @@
 import streamlit as st
 from pyscf import gto, scf
+from streamlit.runtime.scriptrunner import add_script_run_ctx
+import threading
 import time
 
-queue = []
-results = []
+if 'queue' not in st.session_state:
+    st.session_state['queue'] = []
+if 'results' not in st.session_state:
+    st.session_state['results'] = []
+
 
 def compute_pyscf(atom, basis_option, verbose_option):
     print(atom)
@@ -30,6 +35,7 @@ def compute_pyscf(atom, basis_option, verbose_option):
             energy = float(line.split(" ")[-1])
     return energy, time
 
+
 def getMoleculeName(atom):
     d = {}
     for line in atom.split("\n"):
@@ -46,34 +52,65 @@ def getMoleculeName(atom):
         name += key + str(d[key])
     return name
 
+
 # Streamlit layout
 st.title("PySCF")
 
 xyz_input = st.text_area("XYZ Input")
 basis_option = st.selectbox("Basis", ["cc-pVTZ", "asdf"])
-verbose_option = st.selectbox("Verbose", ["3, energy only", "4, cycles and energy", "5, cycles energy and runtime", "9, max"])
+verbose_option = st.selectbox("Verbose", index=2, options=[
+                              "3, energy only", "4, cycles and energy", "5, cycles energy and runtime", "9, max"])
 
-# Create two columns for the buttons
-col1, col2 = st.columns(2, gap="small")
+col1, col2, col3 = st.columns(3, gap="small")
 
-if col1.button("Compute"):
+
+def addToQueue(atom):
+    st.session_state['queue'].append(atom)
+
+
+if col1.button("Add to Queue"):
     if xyz_input:
-        st.write("Computing...")
-        energy, time_val = compute_pyscf(xyz_input, basis_option, verbose_option)
-        
-        # Add the results to your queue and update the UI
-        molecule_name = getMoleculeName(xyz_input)
-        results.append((molecule_name, energy, time_val))
-        st.write(f"Result: {molecule_name} | Energy: {energy} | Time: {time_val} seconds")
+        addToQueue(xyz_input)
     else:
         st.warning("Please provide an XYZ input.")
 
-if results:
-    st.subheader("Results")
-    for result_item in results:
-        st.write(f"{result_item[0]} | Energy: {result_item[1]} | Time: {result_item[2]} seconds")
 
-if col2.button('View Log'):
+if col2.button("Compute"):
+    if len(st.session_state['queue']) > 0:
+        st.write("Computing...")
+        for atom in st.session_state['queue']:
+            energy, time_val = compute_pyscf(
+                atom, basis_option, verbose_option)
+            molecule_name = getMoleculeName(atom)
+            st.session_state['results'].append(
+                (molecule_name, energy, time_val))
+            # st.write(
+            #     f"Result: {molecule_name} | Energy: {energy} | Time: {time_val} seconds")
+    else:
+        st.warning("Please add an XYZ input to the queue.")
+
+if st.session_state['results']:
+    st.subheader("Results")
+    for result_item in st.session_state['results']:
+        st.write(
+            f"{result_item[0]} | Energy: {result_item[1]} | Time: {result_item[2]} seconds")
+
+if col3.button('View Log'):
     with open('computed/output-test.txt', 'r') as file:
         log_data = file.read()
         st.markdown(f'```\n{log_data}\n```')
+
+
+# Attempt at creating an async queue, need to find a way to detect browser closing to stop the queue
+
+# def runQueue():
+#     for i in range(1, 10):
+#         time.sleep(1)
+#         print("test", str(i))
+
+
+# if 'queue-running' not in st.session_state:
+#     st.session_state['queue-running'] = True
+#     t = threading.Thread(target=runQueue)
+#     add_script_run_ctx(t)
+#     t.start()
