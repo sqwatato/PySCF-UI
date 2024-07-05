ad6ab55e5136d1cf9a08d7624d0fdf381bf2628b
ZarseemDyartes <99827309+ZarseemDyartes@users.noreply.github.com>
2024-07-05 16:30:21 -0700

    Update References_and_Acknowledgements.py

--- pages/References_and_Acknowledgements.py
+++ pages/References_and_Acknowledgements.py
@@ -8,7 +8,7 @@ st.set_page_config(
 st.title("References & Acknowlegdements")
 st.write("This UI is free to use and is intended for educational purposes. The researchers involved in this project are affiliated with the Aspiring Scholars Directed Research Program in Fremont, California, and are part of the Quantum Chemistry Group in the McMahan Lab.")
 st.header("References")
-st.write("Sun, Qiming, et al. "PySCF: the Python-based simulations
-of chemistry framework." Wiley Interdisciplinary Reviews:
-Computational Molecular Science, vol. 8, no. 1, 2018,
-e1340. https://doi.org/10.1098/rspa.1951.0048.")
+st.write("""Sun, Qiming, et al. "PySCF: the Python-based simulations
+of chemistry framework." *Wiley Interdisciplinary Reviews:
+Computational Molecular Science*, vol. 8, no. 1, 2018,
+e1340. https://doi.org/10.1098/rspa.1951.0048.""")
