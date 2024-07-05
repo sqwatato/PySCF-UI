24f1408b77672ec03b119aeaa9e5c266f68e9625
ZarseemDyartes <99827309+ZarseemDyartes@users.noreply.github.com>
2024-07-05 16:10:56 -0700

    Update Instructions.py

--- pages/Instructions.py
+++ pages/Instructions.py
@@ -12,4 +12,6 @@ st.write("The UI allows for both custom .xyz file-type molecular geometries, as 
 st.header("Basis Sets")
 st.write("PySCF UI currently allows for users to select any of the basis sets built into the PySCF library. These basis sets are Gaussian-type orbital basis sets (PySCF does not have Slater-type orbital basis sets currently built into the library).")
 st.write("Please note that not all of the basis sets exist for all the atoms; thus, the UI will throw an error if a basis set has not been found for a particular atom. To see the list of all basis sets offered by PySCF, and the atoms for which those basis sets are offered, please visit the link below:")
-st.write("[link]https://pyscf.org/_modules/pyscf/gto/basis.html")  
+st.write("https://pyscf.org/_modules/pyscf/gto/basis.html")  
+st.header("Verbose")
+st.write("The verbose quantity refers to the amount of information displayed in the PySCF logs once the calculation is finished. The minimum verbose level provided by the UI is 3; this is the bare minimum that provides the energy and runtime for the molecule. We recommend to use verbose 5 to get the runtime and energies for each cycle in the SCF calculation process; however, if you would like more data on the process, select a verbose value higher than 5. The maximum verbose value is 9.")
