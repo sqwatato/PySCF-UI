8e30c893f1fab0078806d360fdf090dc45bd1e15
ZarseemDyartes <99827309+ZarseemDyartes@users.noreply.github.com>
2024-07-05 16:04:29 -0700

    Update Instructions.py

--- pages/Instructions.py
+++ pages/Instructions.py
@@ -10,6 +10,6 @@ st.write("This UI is intended to serve as an easy and intuitive way to perform s
 st.header("Selecting Molecular Geometries")
 st.write("The UI allows for both custom .xyz file-type molecular geometries, as well as pre-loaded .xyz geometries. The available pre-loaded geometries can be found under the 'Database' tab, and have been sourced from the CCCBDB NIST database. The custom geometries can be entered in the 'Text Input' or 'File Input' tabs, and must be in the .xyz format.")
 st.header("Basis Sets")
-st.write("PySCF UI currently allows for users to select any of the basis sets built into the PySCF library. These basis sets are Gaussian-type orbital basis sets (PySCF does not have Slater-type orbital basis sets currently built into the library).")\
+st.write("PySCF UI currently allows for users to select any of the basis sets built into the PySCF library. These basis sets are Gaussian-type orbital basis sets (PySCF does not have Slater-type orbital basis sets currently built into the library).")
 st.write("Please note that not all of the basis sets exist for all the atoms; thus, the UI will throw an error if a basis set has not been found for a particular atom. To see the list of all the basis sets offered by PySCF, and the atoms for which those basis sets are included, please visit the link below:"
          
