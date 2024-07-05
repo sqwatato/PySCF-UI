f6db97a147c01358247e809dcfae579513a2f2a2
ZarseemDyartes <99827309+ZarseemDyartes@users.noreply.github.com>
2024-07-05 16:32:31 -0700

    Update References_and_Acknowledgements.py

--- pages/References_and_Acknowledgements.py
+++ pages/References_and_Acknowledgements.py
@@ -8,7 +8,14 @@ st.set_page_config(
 st.title("References & Acknowlegdements")
 st.write("This UI is free to use and is intended for educational purposes. The researchers involved in this project are affiliated with the Aspiring Scholars Directed Research Program in Fremont, California, and are part of the Quantum Chemistry Group in the McMahan Lab.")
 st.header("References")
-st.write("""Sun, Qiming, et al. "PySCF: the Python-based simulations
+st.write("""1. Sun, Qiming, et al. "PySCF: the Python-based simulations
 of chemistry framework." *Wiley Interdisciplinary Reviews:
 Computational Molecular Science*, vol. 8, no. 1, 2018,
 e1340. https://doi.org/10.1098/rspa.1951.0048.""")
+st.write("""2. Sun, Qiming, et al. “Recent developments in the PySCF
+program package.” *The Journal of Chemical Physics*, vol.
+153, no. 024109, 2020. https://doi.org/10.1063/5.0006074.""")
+st.write("""3. NIST Computational Chemistry Comparison and
+Benchmark Database, NIST Standard Reference
+Database Number 101, Release 22, May 2022, edited by
+Russell D. Johnson III. https:// doi.org/10.18434/t47c7z.""")
