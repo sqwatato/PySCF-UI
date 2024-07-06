d48dec06e2a1ff6738ee213f80b3d29039cb270e
ZarseemDyartes <99827309+ZarseemDyartes@users.noreply.github.com>
2024-07-05 23:17:45 -0700

    Update PySCFUI.py

--- PySCFUI.py
+++ PySCFUI.py
@@ -392,7 +392,7 @@ with tab1:
             pd.set_option("display.precision", 16)
             entrodf = pd.DataFrame(entropy, index = ["Total","Electronic","Vibrational","Translational","Rotational"])
             
-            with st.expander(data['Molecule Name'] + "|"+data['Basis']+": " + str(round(data['Real Compute Time'], 2)) + " s"):
+            with st.expander(data['Molecule Name'] + " | "+data['Basis']+": " + str(round(data['Real Compute Time'], 2)) + " s"):
                 result_col_1, result_col_2 = st.columns([2, 1])
                 result_col_1.write(f"SCF CPU Runtime: {data['SCF CPU Runtime']} s")
                 result_col_1.write(f"SCF Wall Runtime: {data['SCF Wall Runtime']} s")
