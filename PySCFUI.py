bed5d90d268ea218a1cf29708803c3b6369d0234
ZarseemDyartes <99827309+ZarseemDyartes@users.noreply.github.com>
2024-07-07 19:16:13 -0700

    Update PySCFUI.py

--- PySCFUI.py
+++ PySCFUI.py
@@ -40,7 +40,7 @@ if 'results' not in st.session_state:
     st.session_state['results'] = []
 if 'computing' not in st.session_state:
     st.session_state['computing'] = False
-
+counter = 0
 
 # get all files in directory names precomputed_molecules:
 precomputed_molecules = list(map(lambda x: x.split(
@@ -345,6 +345,8 @@ if st.button("Compute", disabled=compute_disabled, type="primary", use_container
             data['Smiles'] = smiles
             data['Real Compute Time'] = total_time
             st.session_state['results'].append(data)
+            counter += 1
+            strcounter = str(counter)
             st.rerun()
             
     elif st.session_state['computing'] == True:
@@ -392,7 +394,7 @@ with tab1:
             pd.set_option("display.precision", 16)
             entrodf = pd.DataFrame(entropy, index = ["Total","Electronic","Vibrational","Translational","Rotational"])
             
-            with st.expander(data['Molecule Name'] + " | "+data['Basis']+": " + str(round(data['Real Compute Time'], 2)) + " s"):
+            with st.expander(strcounter + data['Molecule Name'] + " | "+data['Basis']+": " + str(round(data['Real Compute Time'], 2)) + " s"):
                 result_col_1, result_col_2 = st.columns([2, 1])
                 result_col_1.write(f"SCF CPU Runtime: {data['SCF CPU Runtime']} s")
                 result_col_1.write(f"SCF Wall Runtime: {data['SCF Wall Runtime']} s")
